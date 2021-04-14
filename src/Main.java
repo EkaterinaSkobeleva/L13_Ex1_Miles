19cedff1ed0a4ac2d806a2ba20d75d7b04e5a784
Ekaterina Skobeleva <Veritable38210@yandex.ru>
2021-04-14 13:27:07 +0300

    Add new files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 5308.45;
+        double moneyForMile=20;
+        double bonusMiles=ticketPrice/moneyForMile;
+        System.out.println(bonusMiles);
+    }
+}
